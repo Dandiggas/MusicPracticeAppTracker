e570168e15da299abd56fab3f5decad4486029be
Dandiggas <dadekugbe@googlemail.com>
2023-08-11 19:22:31 +0100

    Created base model schema and create user endpoint

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 import psycopg2
 import models
+import schemas
 from sqlalchemy.orm import Session
 from database import engine, SessionLocal
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -22,3 +24,14 @@ async def root():
 @app.get("/sqlalchemy")
 def test_posta(db: Session = Depends(get_db)):
     return {"status": "success"} 
+
+@app.post("/users", status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    new_user=models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+    
---
+++ schemas.py
@@ -0,0 +1,10 @@
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    firstname = str
+    lastname = str
+    email = EmailStr
+    password = str
+ 
